c0856e617db856c445aeaa16806316dbf064552f
roman-yakubovskyi <yakubovskyi133@gmail.com>
2026-03-02 02:03:22 +0100

    add structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
-import {createRoot} from 'react-dom/client';
-import App from "./App.tsx";
+import { createRoot } from 'react-dom/client';
+import App from './components/App/App';
 import './index.css';
 import 'modern-normalize';
 
